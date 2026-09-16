b3a93c894e2873fbc5b96ac80034e0c4d3aee4cb
Kathryn Doering <kathryn.doering@noaa.gov>
2026-09-16 09:32:15 -0700

    Make preview (#197)
    * save first pass at preview site
    
    * fix: indentation
    
    * fix: indentation

--- create_tool_landing_page.py
+++ create_tool_landing_page.py
@@ -88,10 +88,10 @@ def run_all_files(list_of_models, folder_out, configdir, dev_or_prod_config):
         
 
 
-def main(dev_or_prod_config):
+def main(dev_or_prod_config, model_names=None):
     with open("models_all.json", "r") as read_file:
         modellist_configjson = json.load(read_file)
-    list_of_models = modellist_configjson['list_of_models']    
+    list_of_models = model_names or modellist_configjson['list_of_models']
     folder_out= 'deploy'
     configdir= 'model_list_dir'
     run_all_files(list_of_models, folder_out, configdir, dev_or_prod_config)
@@ -100,5 +100,5 @@ def main(dev_or_prod_config):
 if __name__ == '__main__':
     dev_or_prod_config = sys.argv[1]; # use this approach if we need to generate multiple files
     with open(dev_or_prod_config) as f:
-            dev_or_prod_config =  json.load(f)
-    main(dev_or_prod_config)
+        dev_or_prod_config =  json.load(f)
+    main(dev_or_prod_config, sys.argv[2:])
